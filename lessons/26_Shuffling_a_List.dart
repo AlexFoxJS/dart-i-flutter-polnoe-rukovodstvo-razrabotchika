a56ca65f976c5e29becb5e8507eda0681cec8f6e
AlexFoxJS <alexfoxjs@gmail.com>
2018-11-09 20:24:15 +0300

    master: 26 Shuffling a List

---
+++ lessons/26_Shuffling_a_List.dart
@@ -0,0 +1,43 @@
+void main() {
+  var deck = new Deck();
+
+  deck.shuffle();
+  print(deck);
+}
+
+class Deck {
+  List<Card> cards = [];
+
+  Deck() {
+    var ranks = ['One', 'Two', 'Three', 'Four', 'Five'];
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+
+    for (var suit in suits) {
+      for (var rank in ranks) {
+        var card = new Card(rank, suit);
+
+        cards.add(card);
+      }
+    }
+
+  }
+
+  String toString() {
+    return cards.toString();
+  }
+
+  void shuffle() {
+    cards.shuffle();
+  }
+}
+
+class Card {
+  String suit;
+  String rank;
+
+  Card(this.rank, this.suit);
+
+  String toString() {
+    return '\n{rank: "$rank", suit: "$suit"}';
+  }
+}
