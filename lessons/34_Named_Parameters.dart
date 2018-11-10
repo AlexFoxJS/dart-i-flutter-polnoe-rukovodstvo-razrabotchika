1d23c8a6e0a5a9220c0ba3264b7fe03e945383e0
AlexFoxJS <alexfoxjs@gmail.com>
2018-11-10 18:52:52 +0300

    master: 34 Named Parameters

---
+++ lessons/34_Named_Parameters.dart
@@ -0,0 +1,64 @@
+void main() {
+  var deck = new Deck();
+
+  deck.removeCard('Diamonds', 'One');
+  print(deck);
+}
+
+class Deck {
+  List<Card> cards = [];
+
+  Deck() {
+    var ranks = ['One', 'Two', 'Three', 'Four', 'Five'];
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+
+    for (var mySuit in suits) {
+      for (var rank in ranks) {
+        var card = new Card(
+          rank: rank,
+          suit: mySuit
+        );
+
+        cards.add(card);
+      }
+    }
+
+  }
+
+  String toString() {
+    return cards.toString();
+  }
+
+  void shuffle() {
+    cards.shuffle();
+  }
+
+  cardsWithSuits(String suit) {
+    return cards.where((card) => card.suit == suit);
+  }
+
+  deal(int handSize) {
+    var hand = cards.sublist(0, handSize);
+    cards = cards.sublist(handSize);
+
+    return hand;
+  }
+
+  removeCard(String suit, String rank) {
+    cards.removeWhere((card) => (
+      (card.suit == suit) &&
+      (card.rank == rank)
+    ));
+  }
+}
+
+class Card {
+  String suit;
+  String rank;
+
+  Card({this.rank, this.suit});
+
+  String toString() {
+    return '\n{rank: "$rank", suit: "$suit"}';
+  }
+}
