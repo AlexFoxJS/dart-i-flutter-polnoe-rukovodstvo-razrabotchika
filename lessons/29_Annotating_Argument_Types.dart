99b728f7e29409d9321a5eb59c32794a51dae5b5
AlexFoxJS <alexfoxjs@gmail.com>
2018-11-10 18:28:26 +0300

    master: 29 Annotating Argument Types

---
+++ lessons/29_Annotating_Argument_Types.dart
@@ -0,0 +1,47 @@
+void main() {
+  var deck = new Deck();
+
+  deck.shuffle();
+  print(deck.cardsWithSuits('Hearts'));
+}
+
+class Deck {
+  List<Card> cards = [];
+
+  Deck() {
+    var ranks = ['One', 'Two', 'Three', 'Four', 'Five'];
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+
+    for (var suit in suits) {
+      for (var rank in ranks) {
+        var card = new Card(rank, suit);
+
+        cards.add(card);
+      }
+    }
+
+  }
+
+  String toString() {
+    return cards.toString();
+  }
+
+  void shuffle() {
+    cards.shuffle();
+  }
+
+  cardsWithSuits(String suit) {
+    return cards.where((card) => card.suit == suit);
+  }
+}
+
+class Card {
+  String suit;
+  String rank;
+
+  Card(this.rank, this.suit);
+
+  String toString() {
+    return '\n{rank: "$rank", suit: "$suit"}';
+  }
+}
